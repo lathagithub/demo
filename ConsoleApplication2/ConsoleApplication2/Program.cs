e6cc3307aef389957482840101dc65e5a97ff6aa
lathagithub <lathahebeon@gmail.com>
2016-07-20 14:59:15 +0530

    this is my first console appln
    kdfjd kk fj

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//sample line 
+// test changes
+
+            // decclaring tghe variables
+            int x = 200;
+            Console.WriteLine(x);
+            Console.Read();//
+
+
+
+           // int x = 2000;
+            Console.WriteLine("This is a value of x" + x);
+
+        }
+    }
+}
